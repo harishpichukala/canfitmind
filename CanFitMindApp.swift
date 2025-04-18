09c0d200aad23a9c2fac071eebe485df7d0c3732
clogsbasis <clogs.basis.ca@gmail.com>
2025-04-18 13:08:14 -0400

    swift ui push

---
+++ CanFitMindApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct CanFitMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Welcome to CanFitMind")
+                .font(.largeTitle)
+                .padding()
+            Text("Track your fitness with AI")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+        }
+    }
+}
